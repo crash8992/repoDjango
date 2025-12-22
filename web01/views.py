4dc3c3f9d69c64c90ba8b6607a3abb0d89aae8a1
Cristian Bravo <105746182+crash8992@users.noreply.github.com>
2025-12-22 02:58:04 +0000

    aplicando estilos

--- web01/views.py
+++ web01/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import datetime # Importamos una librería de Python
 
 def index(request):
-    return render(request, 'web01/index.html')
+    # Aquí sucede la magia del Backend
+    fecha_actual = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+    
+    # Preparamos el paquete de datos (Contexto)
+    datos = {
+        'nombre_estudiante': 'Cristian Bravo', 
+        'curso': 'Desarrollo en Plataformas Web',
+        'fecha': fecha_actual,
+        'mensaje': 'todo funcionando en orden!'
+    }
+    
+    # Enviamos los datos junto con la plantilla
+    return render(request, 'web01/index.html', datos)
